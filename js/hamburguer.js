2883e4ae7309b75f5031422d8afec8c66ee2ad22
Alejandra <marietsy.911.mm@gmail.com>
2020-12-30 23:40:02 -0300

    Agregado Hamburguer semifuncional

--- js/hamburguer.js
+++ js/hamburguer.js
@@ -1,8 +1,23 @@
-let hamburguer = document.querySelector('#hamburguer');
-let menu = document.querySelector('#menucito');
+let hamburguer = document.querySelector('.menu');
+let menu = document.querySelector('#menu-ham');
+let cerrarHamburguer = document.querySelector('#cerrar-ham');
+let enlaces = document.querySelectorAll('.menucito > ul > li > a');
 
-hamburguer.addEventListener('click', (e)=>{
+const cerrar = (e) =>{
     e.preventDefault();
-    console.log('chlick menú');
-    menu.classList.remove('')
-})
+    menu.classList.remove('menu-ham-activo');
+    menu.classList.add('menu-ham-inactivo');
+}
+
+
+hamburguer.addEventListener('click', (e) =>{
+    e.preventDefault();
+    menu.classList.remove('menu-ham-inactivo');
+    menu.classList.add('menu-ham-activo');
+});
+
+cerrarHamburguer.addEventListener('click', (e)=>{
+    e.preventDefault();
+    menu.classList.remove('menu-ham-activo');
+    menu.classList.add('menu-ham-inactivo');
+});
