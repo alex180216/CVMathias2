9919077cd92d1cdc2508fca863af4f25eae6877e
Alejandra <marietsy.911.mm@gmail.com>
2021-01-01 15:38:19 -0300

    arreglados detalles 2

--- js/changeNavbar.js
+++ js/changeNavbar.js
@@ -1,13 +1,18 @@
 const navbar = document.querySelector('header > .contenedor-nav');
-
-window.onscroll = function() {scrollFunction()};
+/* window.onscroll = scrollFunction();
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    console.log(document.body.scrollTop);
     navbar.style.backgroundColor = '#2a2a2e';
     navbar.style.transition = '1s';
   } else {
     navbar.style.backgroundColor = 'transparent';
     navbar.style.transition = '1s';
   }
-}
+} */
+
+window.onscroll = function() {
+  //var y = window.scrollY;
+  console.log('se mueve');
+};
